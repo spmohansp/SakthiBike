38afd2e03e2fa1ee1f72556cdd0ea3ec3c19ccbb
spmohansp <spmohansp@gmail.com>
2019-02-11 14:30:27 +0530

    first commit

---
+++ database/seeds/LoginSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class LoginSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('admins')->truncate();
+        DB::table('admins')->insert([
+            'name' => 'Admin',
+            'email' => 'admin@admin.com',
+            'password' => bcrypt('123456'),
+        ]);
+    }
+}
