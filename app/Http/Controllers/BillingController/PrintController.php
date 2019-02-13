5f85c152f8872714d52eaa64f626df5002cebea3
spmohansp <spmohansp@gmail.com>
2019-02-13 12:15:15 +0530

    Bill view updated

--- app/Http/Controllers/BillingController/PrintController.php
+++ app/Http/Controllers/BillingController/PrintController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers\BillingController;
+use App\Bill;
 use App\Products;
 use App\Client;
 use Illuminate\Http\Request;
@@ -15,6 +16,27 @@ class PrintController extends Controller
 	}
 
 	public function viewPrint(){
-		return view('admin.print.view_print');
+        $Bills = Bill::orderBy('id', 'DESC')->get();
+		return view('admin.print.view_bills',compact('Bills'));
 	}
+
+    public function saveBill(){
+        $Bill = new Bill;
+        $LastBill = Bill::orderBy('id', 'DESC')->first();
+
+        if (empty($LastBill)){
+            $Bill->bill_number = 1;
+        }else{
+            $Bill->bill_number = $LastBill->bill_number+1;
+        }
+        $Bill->client_id = request('client_id');
+        $Bill->payment_type = request('payment_type');
+        $Bill->date = request('date');
+        $Bill->payment_status = request('payment_status');
+        $Bill->paid_amount = request('paid_amount');
+        $Bill->products = request('products');
+        $Bill->balance_amount = request('balance_amount');
+        $Bill->save();
+        return back()->with('success','Bill Added Successfully!');
+    }
 }
