365ea51c01c69b14bc465647f56cb2253d6bb8a6
Sudhakar Annadurai <sudhakar.annadurai@indusnet.co.in>
2019-02-13 14:00:44 +0530

    ajax changes for bill

--- public/billing/js/bill.js
+++ public/billing/js/bill.js
@@ -1,13 +1,16 @@
 $(document).ready(function () {
-    $("#product_id").change(function () {
-        var product_id = $("#product_id option:selected").val();
-            $.ajax({
-                type: "get",
-                url: '/admin/get_product_data',
-                data: { product_id: product_id },
-                success: function (data) {
-                    console.log(data);
-                }
-            });
+    $("#addbillproduct").click(function () {
+        var product_id = $("#product_id").val();
+        var qty = $("#qty").val();
+        $.ajax({
+            type: 'get',
+            url: '/admin/product/getProduct',
+            data: {
+                product_id: product_id
+            },
+            success: function (data) {
+                console.log(data);
+            }
+        });
     });
 });
--- resources/views/admin/print/add_print.blade.php
+++ resources/views/admin/print/add_print.blade.php
@@ -242,23 +242,5 @@ Add Print
 
         $('#demoSelect').select2();
     </script>
-
-    <script type="text/javascript">
-            $(document).ready(function() {
-                $("#addbillproduct").click(function () {
-                    var product_id = $("#product_id").val();
-                    var qty = $("#qty").val();
-                    $.ajax({
-                        type: 'get',
-                        url: '/admin/product/getProduct',
-                        data:{product_id:product_id},
-                        success:function (data) {
-                            console.log(data);
-                        }
-                    });
-                 });
-            });
-
-    </script>
-
+    
 @endsection
